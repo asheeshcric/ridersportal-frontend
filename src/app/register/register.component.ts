3ef1964dc850731110adbe781ffecc18a81ac285
asheeshcric <ashiz2013@gmail.com>
2018-03-04 12:01:46 +0545

    [ADD] Functionality to the Register Component to register a user to the backend database

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  fname: string;
+  lname: string;
+  email: string;
+  password: string;
+  contact: string;
+  address: string;
+  bike_model: string;
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log("Form Submitted!");
+    this.register().subscribe(result => {
+      console.log(result);
+    });
+  }
+
+  register() {
+    const url = `${environment.api}/register`;
+    return this.http.post(
+      url,
+      {fname: this.fname, lname: this.lname, email: this.email, password: this.password,
+      address: this.address, contact: this.contact, bike_model: this.bike_model}
+    )
+  }
 }
